beba16d955c6da0fc4176498e11d0d4b75367b90
DaMinaup6 <danaup6@gmail.com>
2015-04-18 02:07:39 +0800

    Add: calc of psi vector

--- partA.py
+++ partA.py
@@ -7,7 +7,7 @@ import numpy
 TRAIN_FEATURE_FILENAME 	= "MLDS_HW2_RELEASE_v1/fbank/train_fbank_474.ark"
 TRAIN_LABEL_FILENAME 	= "MLDS_HW2_RELEASE_v1/label/train_474.lab"
 TEST_FEATURE_FILENAME 	= "MLDS_HW2_RELEASE_v1/fbank/test.ark"
-OUTPUT_CSV_FILE_NAME 	= "output/result.csv"
+OUTPUT_CSV_FILE_NAME 	= "output/partA.csv"
 
 trainFeats, trainLabels, trainFrameNames = parse.parseTrainData(TRAIN_FEATURE_FILENAME, TRAIN_LABEL_FILENAME)
 obser = calc.calObservation(trainFeats, labelUtil.labelsToIndices(trainLabels), 69, 48)
